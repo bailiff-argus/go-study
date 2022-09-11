0a60e0351bd589ee5632ca9ac205a0fbe369f95a
bailiff-argus <dmartynenko64@gmail.com>
2022-09-11 20:05:23 +0300

    refactor

--- poster/poster/poster.go
+++ poster/poster/poster.go
@@ -2,32 +2,37 @@ package poster
 
 import (
 	"fmt"
+	"image"
 	"image/jpeg"
 	"net/http"
-    "os"
+	"os"
 )
 
 func SaveImageToFile(imageLink, fileName string) error {
-    // Prepare file
     outFile, err := os.Create(fileName)
     if err != nil { return err }
     defer outFile.Close()
 
-    // Prepare image
-    client := http.Client{}
-    req, err := http.NewRequest("GET", imageLink, nil)
+    image, err := getImage(imageLink)
     if err != nil { return err }
 
-    resp, err := client.Do(req)
-    if err != nil { return err }
-    defer resp.Body.Close()
-
-    if resp.StatusCode != http.StatusOK { return fmt.Errorf("%s", resp.Status)}
-
-    image, err := jpeg.Decode(resp.Body)
-    if err != nil { return err }
-
-    // Write image to file
     err = jpeg.Encode(outFile, image, nil)
     return err
+}
+
+func getImage(url string) (image.Image, error) {
+    client := http.Client{}
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil { return nil, nil }
+
+    resp, err := client.Do(req)
+    if err != nil { return nil, err }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK { return nil, fmt.Errorf("%s", resp.Status)}
+
+    image, err := jpeg.Decode(resp.Body)
+    if err != nil { return nil, err }
+
+    return image, nil
 }
