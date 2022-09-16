343c2515029dc949cce23e5852edec3985abc22e
bailiff-argus <dmartynenko64@gmail.com>
2022-09-16 20:21:13 +0300

    tweaked to create local copy of entered site

--- webcrawler/main.go
+++ webcrawler/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-    "os"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
+	"os"
 
 	"go-study/webcrawler/links"
 	"go-study/webcrawler/search"
@@ -13,10 +16,75 @@ func main() {
     search.BreadthFirst(crawl, os.Args[1:])
 }
 
-func crawl(url string) []string {
-    fmt.Println(url)
-    list, err := links.Extract(url)
+var domain string
+
+func crawl(a string) []string {
+    log.Printf("webcrawler: crawling %s\n", a)
+    // https:// + link.Host + link.Path
+
+    link, err := url.Parse(a)
+    if err != nil { log.Printf("webcrawler: %s", err) }
+
+    if domain == "" {
+        domain = link.Host
+    }
+
+    if link.Host == domain {
+        dir   := "./temp/" + link.Host + link.Path
+        fName := "/temp.html"
+        err := createLocalCopy(a, dir, fName)
+        if err != nil { log.Printf("webcrawler: %s", err) }
+    }
+
+    list, err := links.Extract(a)
     if err != nil { log.Println(err) }
 
     return list
+}
+
+func createLocalCopy(a, folderName, fileName string) error {
+    pgContent, err := getPage(a)
+    if err != nil { return err }
+
+    err = writeContentsToFile(pgContent, folderName, fileName)
+    if err != nil { return err }
+
+    return nil
+}
+
+func writeContentsToFile(text, dir, fileName string) error {
+    err := os.MkdirAll(dir, 0766)
+    if err != nil { return err }
+
+    log.Print("webcrawler: directory creation success")
+
+    file, err := os.Create(dir + fileName)
+    if err != nil { return err }
+
+    log.Printf("webcrawler: %s creation success", file.Name())
+
+    defer file.Close()
+
+    _, err  = file.WriteString(text)
+    if err != nil { return err }
+
+    log.Printf("webcrawler: %s writing success", file.Name())
+
+    return nil
+}
+
+func getPage(a string) (string, error) {
+    resp, err := http.Get(a)
+    if err != nil { return "", err }
+
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return "", fmt.Errorf("cannot fetch %s url: %s", a, resp.Status)
+    }
+
+    body, err := io.ReadAll(resp.Body)
+    if err != nil { return "", err }
+
+    return string(body), nil
 }
